0423bf9d5dff6f7f393b9e345c47d101235e1614
Artem Batura <artemir.q@gmail.com>
2019-01-06 03:01:54 +0300

    improvement(preset.webpack-spa): handle unhandled rejections

--- packages/preset.webpack-spa/bin/cli.js
+++ packages/preset.webpack-spa/bin/cli.js
@@ -3,4 +3,8 @@
 const { runCLI } = require('@zero-scripts/core');
 const { WebpackPresetWeb } = require('../build');
 
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
 runCLI(WebpackPresetWeb);
