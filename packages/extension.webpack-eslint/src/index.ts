4770bc5026c8d0c04bafe02e8b03723a8247c362
Artem Batura <artemir.q@gmail.com>
2019-01-25 20:40:28 +0300

    feature(extension.webpack-eslint): TypeScript support

--- packages/extension.webpack-eslint/src/index.ts
+++ packages/extension.webpack-eslint/src/index.ts
@@ -1,45 +1,93 @@
 import {
   AbstractExtension,
+  ArrayOption,
   extensionsRegex,
+  handleArrayOption,
   InsertPos
 } from '@zero-scripts/core';
-import { WebpackConfig } from '@zero-scripts/config.webpack';
+import {
+  WebpackConfig,
+  WebpackConfigOptions
+} from '@zero-scripts/config.webpack';
 import { resolvePath } from '@zero-scripts/config.webpack';
 
-export class WebpackEslintExtension extends AbstractExtension {
+type WebpackEslintExtensionOptions = {
+  plugins: ArrayOption<string, WebpackConfigOptions>;
+  extends: ArrayOption<string, WebpackConfigOptions>;
+  rules: Record<string, string | any[]>;
+  env: object;
+};
+
+export class WebpackEslintExtension extends AbstractExtension<
+  WebpackEslintExtensionOptions
+> {
   public activate(): void {
-    this.preset.getInstance(WebpackConfig).insertCommonModuleRule(
-      ({ jsFileExtensions, paths }) => ({
+    const config = this.preset.getInstance(WebpackConfig);
+
+    config.insertCommonModuleRule(options => {
+      const { jsFileExtensions, paths } = options;
+      return {
         test: extensionsRegex(jsFileExtensions),
         include: resolvePath(paths.src),
         enforce: 'pre',
-        use: [
-          {
-            loader: require.resolve('eslint-loader'),
-            options: {
-              eslintPath: require.resolve('eslint'),
-              formatter: require.resolve('eslint-formatter-pretty'),
-              baseConfig: {
-                parser: 'babel-eslint',
-                extends: ['eslint:recommended'],
-                plugins: ['import'],
-                env: {
-                  browser: true,
-                  node: true
-                },
+        loader: require.resolve('eslint-loader'),
+        options: {
+          eslintPath: require.resolve('eslint'),
+          formatter: require.resolve('eslint-formatter-pretty'),
+          ignore: false,
+          useEslintrc: false,
+          baseConfig: {
+            parser: 'babel-eslint',
+            extends: [
+              'eslint:recommended',
+              ...handleArrayOption(this.options.extends, options)
+            ],
+            plugins: [
+              'import',
+              ...handleArrayOption(this.options.plugins, options)
+            ],
+            parserOptions: {
+              ecmaVersion: 9,
+              sourceType: 'module',
+              ecmaFeatures: {
+                jsx: true
+              }
+            },
+            env: {
+              browser: true,
+              node: true,
+              ...(this.options.env ? this.options.env : {})
+            },
+            rules: {
+              'no-unused-vars': 'warn',
+              'no-console': 'warn',
+              ...(this.options.rules ? this.options.rules : {})
+            },
+            overrides: [
+              {
+                files: ['*.ts', '*.tsx'],
+                parser: 'eslint-plugin-typescript/parser',
+                plugins: ['typescript'],
                 rules: {
-                  'no-unused-vars': 'warn',
-                  'no-console': 'warn'
+                  'no-undef': 'off',
+                  'no-unused-vars': 'off',
+                  'no-restricted-globals': 'off',
+                  'no-use-before-define': 'off',
+                  'typescript/no-unused-vars': [
+                    'warn',
+                    {
+                      vars: 'all',
+                      args: 'after-used',
+                      ignoreRestSiblings: false
+                    }
+                  ]
                 }
-              },
-              ignore: false,
-              useEslintrc: false
-            }
+              }
+            ]
           }
-        ]
-      }),
-      InsertPos.Start
-    );
+        }
+      };
+    }, InsertPos.Start);
   }
 }
 
