2297fbac6e6a39864e567f51b95adc5cb96c7487
Artem Batura <artemir.q@gmail.com>
2020-08-28 02:49:10 +0300

    Use CSS Modules in react-typescript example

--- examples/react-typescript/src/custom.d.ts
+++ examples/react-typescript/src/custom.d.ts
@@ -2,3 +2,13 @@ declare module "*.svg" {
   const content: any;
   export default content;
 }
+
+declare module "*.m.css" {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module "*.module.css" {
+  const classes: Record<string, string>;
+  export default classes;
+}
