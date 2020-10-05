6b4502fe0e5651f434773877db7fcf1683405868
Artem Batura <artemir.q@gmail.com>
2020-10-05 21:35:57 +0300

    Add example with React 17

---
+++ packages/core/src/cli/__tests__/run.test.ts
@@ -0,0 +1,11 @@
+// import { run } from '../run';
+//
+// describe('core/cli/run', () => {
+//   it('default', () => {
+//     expect(() => {
+//       run(['task'], {
+//         workspace:
+//       });
+//     }).toBe(true);
+//   });
+// });
