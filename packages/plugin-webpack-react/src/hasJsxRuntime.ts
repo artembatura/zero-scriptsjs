3e5b0cd65f079a9a63efb174a3941bfdea4cfd92
Artem Batura <artemir.q@gmail.com>
2020-11-19 03:22:19 +0300

    Add example with React 16 to keep back compatibility Remove extra code

--- packages/plugin-webpack-react/src/hasJsxRuntime.ts
+++ packages/plugin-webpack-react/src/hasJsxRuntime.ts
@@ -1,10 +1,6 @@
 import { WebpackConfigOptions } from '@zero-scripts/webpack-config';
 
 export function hasJsxRuntime(paths: WebpackConfigOptions['paths']): boolean {
-  if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
-    return false;
-  }
-
   try {
     require.resolve('react/jsx-runtime', { paths: [paths.root] });
     return true;
