abfb1ce1f25a6a1391925c6af5a1dc8b7dc2794a
Artem Batura <artemir.q@gmail.com>
2020-11-20 05:29:37 +0300

    Add `--json` option to `build` task which will generate bundle stats file For example, `run build --json` will print stats in stdout For example, `run build --json stats.json` will write stats to `stats.json` file

--- packages/plugin-webpack-spa/src/tasks/TaskBuild.ts
+++ packages/plugin-webpack-spa/src/tasks/TaskBuild.ts
@@ -1,10 +1,16 @@
 import { output } from '@artemir/friendly-errors-webpack-plugin';
-import webpack from 'webpack';
+import { writeFileSync } from 'fs';
+import webpack, { Compiler } from 'webpack';
 
 import { Task } from '@zero-scripts/core';
 import { WebpackConfig } from '@zero-scripts/webpack-config';
 
+import { getStatsOptions } from '../utils/getStatsOptions';
 import { WebpackSpaPluginOptions } from '../WebpackSpaPluginOptions';
+
+type BuildTaskOptions = {
+  json?: boolean | string;
+};
 
 export class TaskBuild extends Task {
   constructor(
@@ -14,18 +20,50 @@ export class TaskBuild extends Task {
     super('build');
   }
 
-  public run(): void | Promise<void> {
+  public run(args: string[], options: BuildTaskOptions): void | Promise<void> {
     process.env.NODE_ENV = 'production';
 
     const config = this.configBuilder.setIsDev(false).build();
+
+    if (options.json) {
+      config.stats = 'verbose';
+    }
 
     const compiler = webpack(config);
 
     output.clearConsole();
     output.title('info', 'WAIT', 'Compiling...');
 
-    compiler.run(err => {
+    compiler.run((err, stats) => {
       if (err) throw err;
+
+      const getStatsOptionsFromCompiler = (compiler: Compiler) =>
+        getStatsOptions(compiler.options ? compiler.options.stats : undefined);
+
+      const foundStats = getStatsOptionsFromCompiler(compiler);
+
+      if (options.json === true) {
+        process.stdout.write(
+          JSON.stringify(stats.toJson(foundStats), null, 2) + '\n'
+        );
+      }
+
+      if (typeof options.json === 'string') {
+        const JSONStats = JSON.stringify(stats.toJson(foundStats), null, 2);
+
+        try {
+          writeFileSync(options.json, JSONStats);
+
+          console.log();
+          output.info(
+            `Stats are successfully stored as json to ${options.json}`
+          );
+        } catch (error) {
+          console.log(error);
+
+          process.exit(2);
+        }
+      }
     });
   }
 }
